0c64be01d8a9e5cdd6ff7822e7b26d9b62f8cc86
Shreyank Joshi <56422917+skjoshi267@users.noreply.github.com>
2020-12-01 14:31:01 +0000

    Added tabulate module
    Added display formatting via tabulate module. Test and Merge if running properly.

--- user_menu/menu.py
+++ user_menu/menu.py
@@ -4,11 +4,26 @@ from errors import Errors
 #Import Stock Data from .CSV file
 from stock_database import stock_data
 
+#Import Tabulate
+import tabulate as tb
+
+#Format the Menu Output based on Menu Type
+def tabulate_output(menu):
+    #Operation Menu GUI v/s CLI
+    if menu.upper() == "OPMENU":
+        header = ["Kindly choose an option to use the app"]
+        table_text = [["1. Graphical User Interface"],["2. Command Line Interface"]]
+    elif menu.upper() == "DISPOPTMENU":
+        header = ["Welcome to Stock Operations!"]
+        table_text = [["Kindly select the operation"],["1. Search & Analyse Stock"], \
+                      ["2. Generate Report"],["3. Terms & Conditions"],["4. Exit"]]
+    return tb.tabulate(table_text,header,"psql")
+
 #Select the mode of application : 1. Command Line 2. GUI
 def user_menu():
-    print("\n"+"-"*50+"\nKindly choose an option to use the app.\n"+"-"*50)
+    operation_menu_txt = tabulate_output("OPMENU")
     try:
-        op_mode = int(input("1. Graphical User Interface \n2. Command Line Interface \nSelect: "))
+        op_mode = int(input(operation_menu_txt+"\nSelect: "))
         print("\n"+Errors.INVALID_CHOICE) if (op_mode != 1 and op_mode != 2) else run_app(op_mode)  
     except ValueError:
         print("\n"+Errors.ONLY_NUMBERS)
@@ -27,10 +42,10 @@ def start_gui():
 #Options for user to select
 def display_options():
     option = 0
+    disp_menu_txt = tabulate_output("DISPOPTMENU")
     while (option != 4):
         try:
-            print("\n"+"-"*30+"\nWelcome to Stock Operations!\nKindly select the operation\n"+"-"*30)
-            option = int(input("1. Search & Analyse Stock \n2. Generate Report \n3. Terms & Conditions \n4. Exit \nSelect: "))
+            option = int(input(disp_menu_txt+"\nSelect: "))
             perform_operation(option)
         except ValueError:
             print("\n"+Errors.ONLY_NUMBERS)
