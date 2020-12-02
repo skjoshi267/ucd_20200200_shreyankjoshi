2a6c99d731e061bd9a16193a8ecde292580cbae5
Shreyank Joshi <sk.joshi267@gmail.com>
2020-12-02 01:49:57 +0000

    Revert "Revert "Completed Merge""
    This reverts commit 32893ae1e10d1c5d4ec523615869c9a856d8cb20.

--- stock_database/data_analysis.py
+++ stock_database/data_analysis.py
@@ -1,4 +1,37 @@
+from config import Configuration
+from errors import Errors,Warnings
+from stock_database.statistics_class import Statistics
+
 def analysis_main(stock_prices):
+<<<<<<< Updated upstream
     pass
     #stock_prices = stock_prices.fillna(0)
     print("\n"+stock_prices["Close"].describe())
+=======
+    analytics_menu_txt = Configuration.tabulate_output("ANMENU")
+    stats_analysis = Statistics(stock_prices)
+    analysis_type = 0
+    while analysis_type != 3:
+        try:
+            analysis_type = int(input("\n"+analytics_menu_txt+"\nSelect: "))
+            print(analysis_type)
+            if analysis_type not in range(1,4):
+                print("\n"+Errors.INVALID_CHOICE)
+                continue
+            elif analysis_type == 1:
+                stats_analysis.desc_stats()
+            elif analysis_type == 3:
+                break
+            else:
+                stats_analysis.pred_stats()
+        except ValueError:
+            print("\n"+Errors.ONLY_NUMBERS)
+        finally:
+            continue
+
+
+    
+    
+
+    
+>>>>>>> Stashed changes
--- stock_database/stock_data.py
+++ stock_database/stock_data.py
@@ -1,43 +1,57 @@
+#Import Pandas
 import pandas as pd
+#Import Configuration
 from config import Configuration
+#Import Errors & Warnings
 from errors import Errors,Warnings
+#Import YFINANCE module
 import yfinance as yf
+#Import URLLIB for handling HTTP Request
 import urllib
+#Import Datetime
 from datetime import datetime
+#Import Data Analysis Functions
 from stock_database import data_analysis
 
+#Clean data before analysis
 def prep_stock_data(stock_data):
     try:
         #Remove unwanted columns
-        stock_data = stock_data.drop(columns = ["Unnamed: 8"],axis = 1)
-        #Specify Missing Values for Industry
-        stock_data.industry = stock_data.industry.fillna("No Industry")
+        stock_data = stock_data.drop(columns = ["Dividends","Stock Splits"],axis = 1)
+        #Fill 0 for null values
+        stock_data = stock_data.fillna(0)
     except:
-        print("\n"+Errors.TRANSFORMATION_ERROR)
-        return stock_data, False
-    else:
-        return stock_data, True
+        print("\n"+Warnings.TRANSFORMATION_ERROR)
+    finally:
+        return stock_data
 
+#Search stock data by yfinance API if Tickr Symbol provided
 def search_stock_api(stock_tickr,period):
     try:
-        #Use Yahoo Finance to get Stock Data for a time period
+        #Replace blank spaces in Symbol
         stock_tickr = stock_tickr.replace(" ","")
         stock_data = yf.Ticker(stock_tickr)
-        print("\n","-"*30,stock_data.info["shortName"],"-"*30,"\n")
-        
         start = None
         end = None
+        #Set period for collecting data
         if period[0] != "" and period[1] != "":
             start = period[0]
             end = period[1]
-            period_arg = None
+            period_arg = ""
         else:
             period_arg = "max"
+<<<<<<< Updated upstream
 
         stock_prices = stock_data.history(period=period_arg,start=start,end=end)
     except OverflowError as invalid_period:
         print("\n"+Errors.CAUGHT_EX.replace("&0",invalid_period))
         return ""
+=======
+        #Get Historical Data
+        stock_prices = stock_data.history(period=period_arg,start=start,end=end)
+        #Perform Data Transformation
+        stock_prices = prep_stock_data(stock_prices)
+>>>>>>> Stashed changes
     except KeyError:
         return ""
     except urllib.error.HTTPError:
@@ -67,6 +81,7 @@ def search_stock_name(stock_data,stock_name):
         print(Errors.UNEXPECTED_ERROR)
         return ""
 
+<<<<<<< Updated upstream
 def get_and_validate_period():
     try:
         valid_start_date = datetime.strptime(input("\nEnter Start Date for Analysis (YYYY-MM-DD): "),"%Y-%m-%d").date()
@@ -83,6 +98,18 @@ def get_and_validate_period():
         period = []
     finally:
         return period
+=======
+def validate_period(period):
+    try:
+        #Convert date string to datetime
+        date_val = datetime.strptime(period,"%Y-%m-%d").date()
+    except ValueError as date_value:
+        print("\n"+Errors.CAUGHT_EX.replace("&0",str(date_value))+ \
+        "\n"+Warnings.CAUGHT_EX.replace("&0","Proceeeding with Max Time Range"))
+        date_val = ""
+    finally:
+        return date_val
+>>>>>>> Stashed changes
 
 def search_stock_data(stock_data,stock_tickr,stock_name):
     #Validate and Get Time Period
@@ -105,19 +132,17 @@ def stock_main():
         #Read the contents of the file
         stock_csv_df = pd.read_csv(Configuration.DATABASE_PATH)
         
-        #Apply Data Transformations
-        stock_csv_df, trans_error = prep_stock_data(stock_csv_df)
-        
         #Search for Stock Data based on Tickr Symbol/Name
         search_choice = 0
         stock_result = ""
+        search_text = Configuration.tabulate_output("SEARCHMENU")
         while search_choice != 3:
             try:
-                search_choice = int(input("\nKindly select an option:\n1. Search by Tickr \n2. Search by Name \
-                \n3. Exit\nSelect: "))      
+                search_choice = int(input("\n"+search_text+"\nSelect: "))      
                 if search_choice == 1:
                     #Search Stock by Ticker
                     stock_tickr = input("\nEnter Stock Tickr Symbol: ")
+                    stock_tickr = stock_tickr.upper()
                     stock_result = search_stock_data(stock_csv_df,stock_tickr,False)
                     
                 elif search_choice == 2:
@@ -140,6 +165,12 @@ def stock_main():
                 else:    
                     print("\n"+Warnings.DATA_NOT_FOUND)
                     continue
+<<<<<<< Updated upstream
+=======
+                else:
+                    #print(Configuration.tabulate_output("DATAFRAME",stock_result))
+                    data_analysis.analysis_main(stock_result)
+>>>>>>> Stashed changes
 
             except ValueError:
                 print("\n"+Errors.ONLY_NUMBERS)
